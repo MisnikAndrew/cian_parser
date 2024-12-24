889c0d7a078703bd57072c5a244436f51532a2f2
MisnikAndrew <misnik.as@phystech.edu>
2024-12-24 19:11:04 +0000

    Add max flat count

--- src/telegram_bot.py
+++ src/telegram_bot.py
@@ -9,6 +9,7 @@ from utils.report_utils import build_report
 from utils.request_utils import get_wait_time
 
 TOKEN = "8188415318:AAG-pRqM4vhL_o5IvGcptB-K9ZHe13Qy53Y"
+max_flat_count = 500
 
 bot = Bot(token=TOKEN)
 storage = MemoryStorage()
@@ -94,30 +95,31 @@ async def process_flat_count(msg: types.Message, state: FSMContext):
     min_room = data.get('min_room')
     max_room = data.get('max_room')
     min_area = data.get('min_area')
-
-    await msg.reply(
-        f'\
-Минимальная стоимость: {min_cost}\n\
-Максимальная стоимость: {max_cost}\n\
-Минимальное количество комнат: {min_room}\n\
-Максимальное количество комнат: {max_room}\n\
-Минимальная площадь: {min_area}\n\
-Количество квартир: {flat_count}\n\
-Начинаю строить отчет, время ожидания ~{get_wait_time(flat_count)} секунд')
-    try:
-        report, csv_file, txt_file = build_report(min_cost, max_cost, min_room, max_room, flat_count, min_area)
-        if flat_count > 3:
-            await msg.reply('Отчет (только первые 3 квартиры):\n' + report)
-        else:
-            await msg.reply('Отчет:\n' + report)
-        await msg.reply('Отчет в формате TXT:')
-        await bot.send_document(msg.from_user.id, txt_file)
-        await msg.reply('Отчет в формате CSV:')
-        await bot.send_document(msg.from_user.id, csv_file)
-        await state.finish()
-    except Exception as e:
-        await msg.reply(f'Произошла ошибка при создании или отправке отчета. Пожалуйста, попробуйте еще раз. Ошибка: {e}')
-
+    if flat_count <= max_flat_count:
+        await msg.reply(
+            f'\
+    Минимальная стоимость: {min_cost}\n\
+    Максимальная стоимость: {max_cost}\n\
+    Минимальное количество комнат: {min_room}\n\
+    Максимальное количество комнат: {max_room}\n\
+    Минимальная площадь: {min_area}\n\
+    Количество квартир: {flat_count}\n\
+    Начинаю строить отчет, время ожидания ~{get_wait_time(flat_count)} секунд')
+        try:
+            report, csv_file, txt_file = build_report(min_cost, max_cost, min_room, max_room, flat_count, min_area)
+            if flat_count > 3:
+                await msg.reply('Отчет (только первые 3 квартиры):\n' + report)
+            else:
+                await msg.reply('Отчет:\n' + report)
+            await msg.reply('Отчет в формате TXT:')
+            await bot.send_document(msg.from_user.id, txt_file)
+            await msg.reply('Отчет в формате CSV:')
+            await bot.send_document(msg.from_user.id, csv_file)
+            await state.finish()
+        except Exception as e:
+            await msg.reply(f'Произошла ошибка при создании или отправке отчета. Пожалуйста, попробуйте еще раз. Ошибка: {e}')
+    else:
+        await msg.reply(f'Вы ввели слишком большое количество квартир, максимальное возможное = {max_flat_count}')
 
 @dp.message_handler(content_types=['text'])
 async def get_text_messages(msg: types.Message):
